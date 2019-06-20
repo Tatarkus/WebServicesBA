dd3dcedd27275ce3d272f800add919b3fb86cdc3
Matías Fuentes Salinas <tarkus@manticore.xj9.com>
2019-06-20 00:56:32 -0400

    Agregado la cabecera para CORS

--- productosAPI/productos.js
+++ productosAPI/productos.js
@@ -1,13 +1,21 @@
 var express    = require('express');
 var app        = express(); 
+var cors = require('cors')
 var port = process.env.PORT || 8080;
 
-app.listen(3000);
+app.use(cors());
+app.listen(port);
 console.log('Iniciado servicio en el puerto: ' + port);
 console.log('Conectandose con la base de datos...');
 
 var pool = require('./conexionbd')
 console.log( pool != null ? "Conexión Exitosa" : "Error en la conexión");
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get("/productos", (req, res, next) => {
 	console.log("Consultando todos los productos");
@@ -24,7 +32,7 @@ app.get("/productos", (req, res, next) => {
 app.get("/productos/:idProducto", (req, res, next) => {
 	console.log("Consultando el producto con id="+req.params.idProducto);
 	pool.query("SELECT * FROM producto where id= ?", [req.params.idProducto])
-  	.then(rows => {
+  	.then(rows => {	
 		res.json(rows);
   		})
   .catch(err => {
