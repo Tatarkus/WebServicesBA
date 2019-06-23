a65c951cf18ea9703e8edb2c1076edaeeea727b2
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2019-06-23 19:20:38 -0400

    api de mantenciones
    obtiene todas las mantenciones de la bd (sqlite) buenos aires y obtiene todas las mantenciones de un usuario

---
+++ despacho_mantencionesAPI/mantenciones.js
@@ -0,0 +1,48 @@
+var express    = require('express');
+var app        = express(); 
+var cors = require('cors')
+var port = process.env.PORT || 8080;
+
+app.use(cors());
+app.listen(port);
+console.log('Iniciado servicio en el puerto: ' + port);
+console.log('Conectandose con la base de datos...');
+
+var db = require('./conexionbd');
+console.log( db != null ? "Conexión Exitosa" : "Error en la conexión");
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get("/mantenciones", (req, res, next) => {
+    db.serialize(() => {
+        db.all(`SELECT * FROM mantencion`, (err, rows) => {
+          if (err) {
+            console.error(err.message);
+          }
+          res.json(rows);
+        });
+     
+      });
+       
+      
+
+});
+
+app.get("/mantenciones/:idUsuario", (req, res, next) => {
+	db.serialize(() => {
+        db.all(`SELECT * FROM mantencion where id_usuario=?`,[req.params.idUsuario], (err, rows) => {
+          if (err) {
+            console.error(err.message);
+          }
+          res.json(rows);
+        });
+
+    });
+
+	
+
+});
