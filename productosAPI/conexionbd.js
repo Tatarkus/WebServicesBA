63706b76acf4d327b1d99cc2ee32cd761f5d00b1
Tatarkus <matias.trks@gmail.com>
2019-06-24 18:35:55 -0400

    usando process.env

--- productosAPI/conexionbd.js
+++ productosAPI/conexionbd.js
@@ -2,7 +2,7 @@ var mariadb = require('mariadb');
 require('dotenv').config();
 
 var pool = mariadb.createPool({
-  host: "localhost",
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "avaras08",
   database: process.env.DB_NAME || "anwo",
