c5c1313096ec9f89feaf40fe0264a12d5fe659fa
Tatarkus <matias.trks@gmail.com>
2019-06-24 17:24:53 -0400

    Update conexionbd.js

--- productosAPI/conexionbd.js
+++ productosAPI/conexionbd.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 var pool = mariadb.createPool({
   host: "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "avaras08",
+  database: process.env.DB_NAME || "anwo",
   connectionLimit: 5
 });
 
